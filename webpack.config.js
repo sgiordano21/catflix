7874be6b17ebda38953df0f61974f7467e904a7e
Student <example@example.com>
2019-11-13 16:42:53 -0800

    update title

--- webpack.config.js
+++ webpack.config.js
@@ -72,7 +72,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'template.ejs',
       appMountId: 'react-app-root',
-      title: 'React Help Queue',
+      title: 'Catflix',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
